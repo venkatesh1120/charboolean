d32b19c0a5441d12932c300c8d3a85ad072b148a
happy <venkatesh1120@gmail.com>
2019-02-25 11:25:55 +0100

    Initial commit

---
+++ src/com/venky/Main.java
@@ -0,0 +1,11 @@
+package com.venky;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //max width is 16 (2bytes)
+        char logo = '\u00A9';
+        char reg = '\u00AE';
+        System.out.println("logo cpoyright = " +logo  +reg);
+    }
+}
